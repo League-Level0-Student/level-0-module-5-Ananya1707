bd840adb2d9c0fd99b4ed82ec42d6512d4fa0ee9
root <root@47c5ee145873>
2020-08-17 23:58:10 +0000

    Working on Snowflake Project

--- src/_01_algorithms/_2_fibonacci/Fibonacci.java
+++ src/_01_algorithms/_2_fibonacci/Fibonacci.java
@@ -4,6 +4,14 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		int number1 = 0;
 		int number2 = 1;
-		int sum;
+		int sum = 0;
+		System.out.println(sum);
+		
+		for(int i = 0; i < 10; i++) {
+			sum = number1 + number2; 
+			number2 = number1;
+			number1 = sum;
+			System.out.println(sum);
+		}
 	}
 }
